5d8ecd63f7dab337e376783b3d3f68d258d671c5
YavuzGurdal <57366318+YavuzGurdal@users.noreply.github.com>
2021-12-15 01:38:05 +0100

    Update currency.js

--- currency.js
+++ currency.js
@@ -6,27 +6,38 @@ var day = new Date().getDate();
 var month = new Date().getMonth();
 console.log(month);
 var year = new Date().getFullYear();
-var months = ["January","February","March","April","May","June","July","August","September","Oktober","Nowember","December"];
+var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "Oktober",
+    "Nowember",
+    "December",
+];
 
-document.getElementById("dateInfo").innerHTML = `${day} ${months[month]} ${year}`;
+document.getElementById(
+    "dateInfo"
+).innerHTML = `${day} ${months[month]} ${year}`;
 
-setInterval( () => {
+setInterval(() => {
     var loctime = new Date().toLocaleTimeString();
     // console.log('loctime :', loctime)
-    document.getElementById("localTime").innerHTML = loctime
+    document.getElementById("localTime").innerHTML = loctime;
 }, 1000);
 
+document.getElementById("submit").addEventListener("click", calculateCurrency);
 
+function calculateCurrency() {
+    var mevcut1 = $("#doviz").val();
+    var cevrilecek1 = $("#cevrilecekDoviz").val();
 
-
-document.getElementById('submit').addEventListener('click', calculateCurrency);
-
-function calculateCurrency () {
-
-    var mevcut1 = $( "#doviz" ).val();
-    var cevrilecek1 = $( "#cevrilecekDoviz" ).val();
-
-    var tutarDoviz = document.getElementById('tutar').value;
+    var tutarDoviz = document.getElementById("tutar").value;
 
     console.log(mevcut1);
     console.log(cevrilecek1);
@@ -35,33 +46,46 @@ function calculateCurrency () {
     // & ile istediğimiz kadar parametre verebiliyoruz
 
     var url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${mevcut1}&to_currency=${cevrilecek1}&apikey=MMVOSLDS608KR66B`;
-                                                         
+
     fetch(url)
-        .then( response => response.json())
-        .then( data => {
+        .then((response) => response.json())
+        .then((data) => {
             console.log(data);
             //document.getElementById('sorguekrani').style.display = 'none';
 
-            var fromCurrencyCode = data["Realtime Currency Exchange Rate"]["1. From_Currency Code"];
-            var fromCurrencyName = data["Realtime Currency Exchange Rate"]["2. From_Currency Name"];
+            var fromCurrencyCode =
+                data["Realtime Currency Exchange Rate"]["1. From_Currency Code"];
+            var fromCurrencyName =
+                data["Realtime Currency Exchange Rate"]["2. From_Currency Name"];
 
-            var toCurrencyCode = data["Realtime Currency Exchange Rate"]["3. To_Currency Code"];
-            var toCurrencyName = data["Realtime Currency Exchange Rate"]["4. To_Currency Name"];
+            var toCurrencyCode =
+                data["Realtime Currency Exchange Rate"]["3. To_Currency Code"];
+            var toCurrencyName =
+                data["Realtime Currency Exchange Rate"]["4. To_Currency Name"];
 
-            var exchangeRate = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+            var exchangeRate =
+                data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
 
-            var lastRefreshed = data["Realtime Currency Exchange Rate"]["6. Last Refreshed"]
+            var lastRefreshed =
+                data["Realtime Currency Exchange Rate"]["6. Last Refreshed"];
 
-            var sonuc = (tutarDoviz*exchangeRate).toFixed(2);
+            var sonuc = (tutarDoviz * exchangeRate).toFixed(2);
 
+            var exchangeRateFixed = (exchangeRate * 1).toFixed(2);
 
             // to be able to show div block (display: block)
-            document.getElementById('show').style.display = 'block';
+            document.getElementById("show").style.display = "block";
 
-            document.getElementById('alt').innerHTML = `1 ${fromCurrencyCode}  =  ${exchangeRate} ${toCurrencyCode}`;
+            document.getElementById(
+                "alt"
+            ).innerHTML = `Exchange Rate :  1 ${fromCurrencyCode}  =  ${exchangeRateFixed} ${toCurrencyCode}`;
 
-            document.getElementById('tutarAlt').innerHTML = `Tutar : ${tutarDoviz} ${fromCurrencyCode}`;
-            document.getElementById('cevrilenAlt').innerHTML = `Çevrilen Tutar : ${sonuc} ${toCurrencyCode}`;
+            document.getElementById(
+                "tutarAlt"
+            ).innerHTML = `${tutarDoviz} ${fromCurrencyCode} = ${sonuc} ${toCurrencyCode}`;
+            // document.getElementById(
+            //     "cevrilenAlt"
+            // ).innerHTML = `Çevrilen Tutar : ${sonuc} ${toCurrencyCode}`;
         })
-        .catch( error => console.log(error))       
+        .catch((error) => console.log(error));
 }
